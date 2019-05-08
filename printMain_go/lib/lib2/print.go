4c986289fee6006dcb31258d3e5dff5a151a29de
blueJpg <2238288979@qq.com>
2019-05-08 22:25:07 +0800

    2019-5-8

---
+++ printMain_go/lib/lib2/print.go
@@ -0,0 +1,9 @@
+package lib2
+
+import (
+    "fmt"
+)
+
+func Print(name string) {
+    fmt.Printf("hello, %s!\n", name)
+}
---
+++ printMain_go/printMain.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "printMain_go/lib"
+    "printMain_go/lib/lib2"
+)
+
+func main() {
+    name := "hello"
+    lib1.Print(name)
+    lib2.Print(name)
+}
